d293246f7466c171a7c5465c4cd97eab019a6669
Jarosław Frydrych <jarek.frydrych@gmail.com>
2017-10-26 14:05:16 +0200

    photos

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,6 +5,8 @@
 
 import jQuery from 'jquery';
 import * as AOS from 'aos';
+import 'babel-core/register';
+import 'babel-polyfill';
 
 const $ = jQuery;
 window.jQuery = window.$ = $;
@@ -65,28 +67,28 @@ class Text {
     this.i = 0;
   }
 
-  typing(_text, speed) {
-    //this.textContainer.text(text);
-    const len = _text.length;
-    let i = 0;
-    let text = '';
-    this.speed = speed;
-    this.text = _text;
-    this.len = _text.length;
+  async typing(_text, speed) {
+    return new Promise(resolve => {
+      this.speed = speed;
+      this.text = _text;
+      this.len = _text.length;
 
-    this.type();
+      this.type(() => {
+        return resolve();
+      });
+    });
   }
 
-  type() {
-    if(this.i >= this.len)
-      return;
+  type(callback) {
+    if (this.i >= this.len)
+      return callback();
 
     setTimeout(() => {
-      if(typeof this.text[this.i] !== 'undefined') {
+      if (typeof this.text[this.i] !== 'undefined') {
         this.typedText += this.text[this.i];
         this.i++;
         this.setText(this.typedText);
-        this.type();
+        this.type(callback);
       }
     }, this.variableSpeed(this.speed));
   }
@@ -106,17 +108,17 @@ class Scene2 {
     this.element = $('#' + element);
   }
 
-  start(colors) {
-    this.colors = colors;
+  start() {
     Main.show(this.element);
-    for(let i = 1; i<=60; i++) {
+    for (let i = 1; i <= 78; i++) {
       this.fillPhotos(i);
     }
 
     setTimeout(() => {
-      AOS.refreshHard();
+      AOS.refresh();
       window.dispatchEvent(new Event('resize'));
-    }, 100);
+      console.log('resize');
+    }, 1000);
   }
 
   fillPhotos(i) {
@@ -127,13 +129,17 @@ class Scene2 {
       </div>
     </div>`);
   }
-
 }
 
-$(document).ready(() => {
-  AOS.init();
+class Sequence {
+  constructor() {
+    this.run()
+      .then(() => {
+        console.log('all done');
+      });
+  }
 
-  setTimeout(() => {
+  async run() {
     const main = new Main('main');
     const logoHero = $('#logoHero');
     const text1 = new Text('timeTo');
@@ -141,49 +147,55 @@ $(document).ready(() => {
     const scene2 = new Scene2('scene2');
     let hidePointer = false;
     let clicked = false;
-    const colors = [
-      '0e76bc', '283891', 'f1592a', 'ed217c', '662d91', 'ed1c24', 'fbc22c', 'f7941d'
-    ];
+    let typedText1 = 'time to say goodbye...';
 
-    setTimeout(() => {
-      let typedText1 = 'time to say goodbye...';
-      text1.typing(typedText1, 150);
+    await this.timeout(3000);
+    await text1.typing(typedText1, 150);
+    await this.timeout(2000);
+    typedText1 += ' Plej';
+    await text1.typing(typedText1, 150);
+
+    logoHero.addClass('active');
+    logoHero.mouseover(() => {
+      console.log('over');
+      if (!hidePointer)
+        Main.hide(pointer);
+
+      hidePointer = true;
+    });
+
+    logoHero.on('click', () => {
+      logoHero.addClass('animated zoomOut');
+      clicked = true;
 
       setTimeout(() => {
-        typedText1 += ' Plej';
-        text1.typing(typedText1, 150);
+        logoHero.hide();
 
-        logoHero.addClass('active');
-        logoHero.mouseover(() => {
-          console.log('over');
-          if(!hidePointer)
-            Main.hide(pointer);
+        main.hideMain();
+        scene2.start();
+      }, 500)
+    });
 
-          hidePointer = true;
-        });
+    await this.timeout(3000);
+    if (!hidePointer || !clicked) {
+      Main.show(pointer);
+    }
+  }
 
-        logoHero.on('click', function () {
-          logoHero.addClass('animated zoomOut');
-          clicked = true;
+  async timeout(delay) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        return resolve();
+      }, delay);
+    });
+  }
+}
 
-          setTimeout(() => {
-            logoHero.hide();
+$(document).ready(() => {
+  AOS.init();
 
-            main.hideMain();
-            scene2.start(colors);
-          }, 500)
-        });
-
-        setTimeout(() => {
-          if(hidePointer || clicked)
-            return;
-
-          Main.show(pointer);
-        }, 3000);
-
-      }, 7000);
-    }, 3000);
-
+  setTimeout(() => {
+    new Sequence();
   }, 500);
 });
 
